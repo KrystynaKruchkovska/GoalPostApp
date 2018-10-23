b55ea7bc464e6504d266d0af03fa07ef947f24b4
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-10-23 10:53:54 +0300

    bindTokeyBoardUIView/UIButtonExt

---
+++ GoalsPost/Extensions/UIButtonExt.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExt.swift
+//  GoalsPost
+//
+//  Created by Mac on 10/23/18.
+//  Copyright © 2018 CO.KrystynaKruchcovska. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setSelectedColor(){
+        self.backgroundColor = #colorLiteral(red: 0.4922404289, green: 0.7722371817, blue: 0.4631441236, alpha: 1)
+    }
+    func setDeselectedColor(){
+        self.backgroundColor = #colorLiteral(red: 0.5561258793, green: 0.7782232165, blue: 0.6976922154, alpha: 1)
+        
+    }
+}
